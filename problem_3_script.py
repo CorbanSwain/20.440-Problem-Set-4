bfee723a2a5d7d81cb7b51923b4c29adcf57e38b
CorbanSwain <CorbanSwain@gmail.com>
2018-03-04 16:07:48 -0500

    added part 5 implementation, small change

--- problem_3_script.py
+++ problem_3_script.py
@@ -1,7 +1,12 @@
 #!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
+# problem_3_sctipt.py
+# Corban Swain, March 2018
 
+# importing required packages
 import numpy as np
 import pprint as pp
+
+
 
 ## Loading in Data ##
 # protein sequences
@@ -10,8 +15,11 @@ prots = [['G', 'I', 'G', 'V', 'D', 'D', 'V', 'I', 'K', 'L'],
          ['G', 'L', 'G', 'V', 'F', 'D', 'A', 'I', 'S', 'L'],
          ['Q', 'L', 'G', 'I', 'N', 'D', 'Q', 'I', 'S', 'M'],
          ['G', 'I', 'G', 'K', 'Q', 'N', 'E', 'V', 'S', 'L']]
+
+# convert to numpy array for easier slicing
 prots = np.array(prots)
 
+# number of sequences
 M = len(prots)
 
 # amino acid backgroud frequencies
@@ -24,39 +32,50 @@ q_dict = {'A' : 0.073, 'C' : 0.025, 'D' : 0.050, 'E' : 0.061,
 # list of amino acids
 aas = aa_freq_dict.keys()
 
+
+
 ## Part 1 ##
+def aa_freqs(seqs, pos):
+    # number of sequences
+    M = len(seqs)
+    
+    # constant to save a bit of computation time
+    freq_per_count = 1.0 / M
+
+    # initialize empty dictionary
+    freq_dict = {}
+    
+    # looping through amino acids in each position
+    for aa in seqs[:, pos]:
+        # get the number of counts for that aa in the
+        # dictionary, zero if not present
+        current_count = freq_dict.get(aa, 0.0)
+
+        # increase the count by one
+        freq_dict[aa] = current_count + freq_per_count
+
+    return freq_dict
+
 # positions to analyze (zero indexed)
 positions = [0, 4, 9]
 
 # list of dictionaries to keep track of counts
 freqs_out = []
 
-# constant to save a bit of computation time
-freq_per_count = 1.0 / M
-             
 # looping through queried positions
 for pos in positions:
-    # temporary dictionary to store counts of each aa
+    # calculating the frequenciec for each aa at 'pos'
     # in the MSA
-    temp_dict = {}
+    freqs_out.append(aa_freqs(prots, pos))
 
-    # looping through amino acids in each position
-    for aa in prots[:, pos]:
-        # get the number of counts for that aa in the
-        # dictionary, zero if not present
-        current_count = temp_dict.get(aa, 0.0)
-
-        # increase the count by one
-        temp_dict[aa] = current_count + freq_per_count
-
-    # appending the dict to the ouput cache
-    freqs_out.append(temp_dict)
 
 # printing the calulated output
 print('\nPart 1: AA Frequencies')        
 pp.pprint(freqs_out)
 
-## Part 2 and 3 ##
+
+
+## Parts 2 and 3 ##
 # useful math functions
 fact = np.math.factorial
 ln = np.math.log
@@ -69,27 +88,25 @@ M_fact = fact(M)
 # amino acid at a specific frequency withing a set of aligned
 # sequences. Formulated according to Ranganathan Lab - Note 109:
 # http://systems.swmed.edu/rr_lab/Note109_files/Note109_v3.html
-def p_observed(aa, freq):
+def p_observed(aa, freq, M):
     counts = round(M * freq)
     noncounts = M - counts
     q = q_dict[aa]
-    
     temp_1 = M_fact / (fact(counts) * fact(noncounts))
     temp_2 = q ** counts
     temp_3 = (1 - q) ** noncounts
     return temp_1 * temp_2 * temp_3
 
 # relative entropy calulation
-def rel_entropy(aa, freq):
+def rel_entropy(aa, freq, M):
     nonfreq = 1 - freq
     q = q_dict[aa]
-    
     temp_1 = freq * ln(freq / q)
     temp_2 = nonfreq * ln(nonfreq / (1 - q))
     return temp_1 + temp_2
 
-# uses sterlings approximation    
-def p_observed_approx(aa, freq):
+# uses Sterling's approximation    
+def p_observed_approx(aa, freq, M):
     D = rel_entropy(aa, freq)
     return exp(-M * D)
 
@@ -107,14 +124,50 @@ for obsv_freqs in freqs_out:
     for aa, freq in obsv_freqs.items():
         # calulating probability of observing each aa it its
         # present frequency
-        p_obsv_out[-1][aa] = p_observed(aa, freq)
+        p_obsv_out[-1][aa] = p_observed(aa, freq, M)
 
-        # calulating the relative entropy assosiated wit
+        # calulating the relative entropy assosiated with
         # observing each aa at its present frequency
-        entropy_out[-1][aa] = rel_entropy(aa, freq)
+        entropy_out[-1][aa] = rel_entropy(aa, freq, M)
         
 # printing the calulated output
 print('\nPart 2: Probabilities of Observation')        
 pp.pprint(p_obsv_out)
 print('\nPart 3: Relative Entropy for Observation')        
 pp.pprint(entropy_out)
+
+
+
+## Part 4 ##
+# A relative entropy of zero represents the case when an amino acid
+# at a given position in the multiple sequence alighment appears with
+# frequency equivalent to the backgroud frequency for that amino acid.
+
+
+
+## Part 5 ##
+def coupling_E(seqs, aa1, pos1, aa2, pos2):
+    # number of protein sequences
+    M = len(seqs)
+    
+    # frequency of aa2 at pos2 in the MSA
+    freq = aa_freqs(seqs, pos2).get(aa2, 0.0)
+
+    # subset of the sequences with aa1 at pos1
+    enriched_seqs = [s for s in seqs if s[pos1] == aa1]
+    enriched_seqs = np.array(enriched_seqs)
+
+    # frequency of aa2 at pos2 in the subset
+    coupled_freq = aa_freqs(enriched_seqs, pos2).get(aa2, 0.0)
+
+    # calculating the statistical coupling energy
+    temp1 = ln(p_observed(aa2, freq, M))
+    temp2 = ln(p_observed(aa2, coupled_freq, len(enriched_seqs)))
+    return - 1.0 / M * (temp1 - temp2)
+
+# performing the calculation for the spcified inputs
+ddG = coupling_E(prots, 'G', 0, 'L', 9)
+
+# printing the calculated output
+print('\nPart 5: Statistical Coupling Energy')        
+print('\Delta\DeltaG = %f' % ddG)
